1f2e3cad6a19a79936494195eaa67ac52e81fa33
Baptiste Gislot <baptiste.gislot@gmail.com>
2021-01-05 11:44:44 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+process.on("SIGINT", () => {
+  console.log("Caught interrupt signal");
+  process.exit();
+});
+
+process.on("SIGTERM", () => {
+  console.log("Caught interrupt signal");
+  process.exit();
+});
+
+(async () => {
+  const app = express();
+
+  console.log(`My super secret is: ${process.env.SUPER_SECCRET}`);
+  console.log("Connecting to MongoDB database");
+  await mongoose.connect("mongodb://mongodb:27017/ew-docker", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  });
+  console.log("Connected");
+
+  app.get("/", (req, res) => {
+    console.log("Got a request");
+    res.json({ message: "Hey, I'm Tom, the API" });
+  });
+
+  app.listen(3000, () => {
+    console.log("Server is running");
+  });
+})();
